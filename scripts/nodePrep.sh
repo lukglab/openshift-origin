7997b0455817a102955291e78ec50979310fb1fd
Łukasz Głąb <lglab@future-processing.com>
2020-05-07 23:29:19 +0200

    update to 3.11

--- scripts/nodePrep.sh
+++ scripts/nodePrep.sh
@@ -45,7 +45,7 @@ echo $(date) " - Installing Docker"
 
 yum install docker
 sed -i -e "s#^OPTIONS='--selinux-enabled'#OPTIONS='--selinux-enabled --insecure-registry 172.30.0.0/16'#" /etc/sysconfig/docker
-
+yum install -y container-storage-setup
 echo $(date) " - Docker installed successfully"
 
 # Create thin pool logical volume for Docker
